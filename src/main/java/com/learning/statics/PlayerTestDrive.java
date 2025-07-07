07892b723e59975b544b7c3d168895855c7b9280
Manish <manishik@users.noreply.github.com>
2025-07-07 15:36:10 -0400

    Update PlayerTestDrive.java

--- src/main/java/com/learning/statics/PlayerTestDrive.java
+++ src/main/java/com/learning/statics/PlayerTestDrive.java
@@ -1,44 +1,52 @@
 package com.learning.statics;
 
 public class PlayerTestDrive {
-	
-	public static void main(String[] args) {
-		
-		System.out.println(Player.playerCount);
-		
-		Player one = new Player("Tiger Woods");
-		System.out.println(Player.playerCount);
-		
-		Player two = new Player("Nadal");
-		System.out.println(Player.playerCount);
-		
-		two.play();
-		go();
-	}
-	
-	static public void go(){
-		System.out.println("Hey it's done");
+
+    public static void main(String[] args) {
+		System.out.println("Inside main method...");
+        System.out.println("Player.playerCount = " + Player.playerCount);
+
+        Player one = new Player("Tiger Woods");
+        System.out.println("Player.playerCount = " + Player.playerCount);
+		System.out.println("one.count = " + one.count);
+
+        Player two = new Player("Nadal");
+        System.out.println("Player.playerCount = " + Player.playerCount);
+		System.out.println("two.count = " + two.count);
+
+        two.play();
+        go();
+    }
+
+    public static void go() {
+        System.out.println("Hey it's done");
+    }
+
+	static {
+		System.out.println("Inside static block...");
 	}
 
 }
 
 
-class Player{
-	static int playerCount;
-	int count;
-	public String name;
-	Player(String n){
-		name = n;
-		playerCount++;
-		count++;
-	}
-	
-	public void play(){
-		System.out.println("Player:play()");
-		System.out.println("playercount = "+playerCount);
-		Player three = new Player("Manish");
-		Player four = new Player("Sandesh");
-		Player five = new Player("Kiran");
-		System.out.println("count = "+five.count);
-	}
+class Player {
+    static int playerCount;
+    int count;
+	String name;
+
+    Player(String name) {
+        this.name = name;
+        playerCount++;
+        count++;
+    }
+
+    public void play() {
+		System.out.println();
+        System.out.println("Player:play()");
+        System.out.println("playerCount = " + playerCount);
+        Player three = new Player("Manish");
+        Player four = new Player("Sandesh");
+        Player five = new Player("Kiran");
+        System.out.println("Count = " + five.count);
+    }
 }

574013b0f8a3e317286f539c8c4746a41ddecd7c
Manish <manishik@users.noreply.github.com>
2026-05-31 16:51:40 -0400

    minor

--- src/main/java/com/javaInterview/arrays/ArrayOperations.java
+++ src/main/java/com/javaInterview/arrays/ArrayOperations.java
@@ -9,9 +9,10 @@ public class ArrayOperations {
 
 
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5};
-        System.out.println("Array Numbers: " + Arrays.toString(numbers));
+        //int[] numbers = {1, 2, 3, 4, 5};
+        //System.out.println("Array Numbers: " + Arrays.toString(numbers));
         int[] arr = new int[]{1, 2, 3, 4, 2, 4};
+        System.out.println("Array Numbers: " + Arrays.toString(arr));
         int first = arr[0];
         System.out.println("First element is: " + first);
         System.out.println("Second element is: " + arr[2]);
@@ -19,7 +20,7 @@ public class ArrayOperations {
         System.out.println("Array Length = " + arr.length);
 
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
         Arrays.stream(arr).forEach(System.out::println);
@@ -30,6 +31,11 @@ public class ArrayOperations {
         Arrays.sort(arr);
         System.out.println("Sorted Array: " + Arrays.toString(arr));
 
+        // Sorting int array in reverse order
+        int[] reveredSortedArr = Arrays.stream(arr).boxed().sorted(Collections.reverseOrder()).mapToInt(Integer::intValue).toArray();
+        System.out.println("Revered Sorted Array: " + Arrays.toString(reveredSortedArr));
+
+        System.out.println();
         Integer[] array = {5, 2, 9};
         System.out.println("Integer Array Elements: " + Arrays.toString(array));
         Arrays.sort(array);
@@ -59,7 +65,7 @@ public class ArrayOperations {
         System.out.println("Doubled: " + Arrays.toString(doubled));
 
         int[] unique = Arrays.stream(arr).distinct().toArray();
-        System.out.println("Unique: " + Arrays.toString(unique));
+        System.out.println("Unique Elements in Array : " + Arrays.toString(unique));
 
         long count = Arrays.stream(arr).filter(n -> n < 10).count();
         System.out.println("Count of elements <10 : " + count);
